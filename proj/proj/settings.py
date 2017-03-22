b6e49b3bafee5986b2ff04566689947efed77b62
mmatusiak <azamennace@gmail.com>
2017-03-22 17:52:42 +0100

    u

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shell',
+    'web',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- proj/proj/urls.py
+++ proj/proj/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('shell.urls'),
+    url(r'^home/', include('web.urls'))
 ]
