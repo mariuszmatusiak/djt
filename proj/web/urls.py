693b819c97d531ef30e5d4b03ceda8dc5c1a01b7
djsz-30 <206385@edu.p.lodz.pl>
2017-03-27 08:11:38 +0200

    u

--- proj/web/urls.py
+++ proj/web/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^index$', views.index, name='index'),
+    url(r'^contact$', views.contact, name='contact'),
+    url(r'^blog$', views.blog, name='blog'),
+    url(r'^projects$', views.projects, name='projects')
 ]
--- proj/web/views.py
+++ proj/web/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'web/home.html')#HttpResponse("Hello")
+	
+def contact(request):
+    return render(request, 'web/contact.html')#HttpResponse("Hello")
+
+def projects(request):
+    return render(request, 'web/notfound.html')#HttpResponse("Hello")
+
+def blog(request):
+    return render(request, 'web/notfound.html')#HttpResponse("Hello")
+
